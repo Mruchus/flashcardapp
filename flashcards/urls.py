9d2adee1c8db290b8f3d1e9e4abd55e741ce8514
mruchus <mruchus@icloud.com>
2022-12-30 12:11:25 +0000

    step through cards; create new cards when reach end

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('text-file/', views.display_text_file, name='display_text_file'),
     path('cards/', views.display_cards, name='display_cards'),
-    path('flashcard/<int:card_id>/', views.card_detail, name='detail'),
-    # ex: /polls/5/results/
-    # defines a URL pattern that maps the /text-file/ URL to the display_text_file view function.
+    path('flashcard/<int:card_id>/', views.card_detail, name='display_card'),
+    path("flashcard/new",views.CardCreateView.as_view(),name="create_card"),
+    # will look for card_form.html template by default
+   
 ]
-# 'name' can be used later to refer specific views/
+# 'name' can be used later to refer specific views
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Card
-from django.views import generic
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+)
+
 # class-based view -> allows you to display a list or detail page for a model
 
 
@@ -18,12 +22,57 @@ def display_cards(request):
     return render(request, 'cards.html', {'cards': cards}) # renders using the cards template
 
 def card_detail(request, card_id):
-    card_num = len(Card.objects.all())
+    num_cards = Card.objects.count()
     try:
         card = Card.objects.get(pk=card_id)
+        cards = Card.objects.all()
+        #print(cards)
+        for index, obj in enumerate(cards):
+            if obj == card:
+                current_i = index
+        
+        start_of_cards = False
+        end_of_cards = False
+        
+        if current_i == 0:
+            start_of_cards = True
+            previous_id = None
+            next_id = cards[current_i+1].id
+
+        if current_i == num_cards - 1:
+            end_of_cards = True
+            previous_id = cards[current_i-1].id
+            next_id = None
+        
+        elif current_i != 0:
+            previous_id = cards[current_i-1].id
+            next_id = cards[current_i+1].id
+
     except Card.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'card_detail.html', {'card': card})
+    return render(request, 'card_detail.html',
+    {'card': card, 'num_of_cards':num_cards,
+    'previous_id': previous_id, 'next_id': next_id,
+    'start_of_cards': start_of_cards, 'end_of_cards': end_of_cards})
+
+# form to add new flashcards
+class CardCreateView(CreateView):
+    model = Card
+    fields = ["question", "answer"] # fields for the form
+    # after form successfully completed, return to creating more cards
+    success_url = reverse_lazy("create_card") 
+
+    def get_context_data(self, **kwargs): 
+        'add additional context variables to the template context'
+        context = super().get_context_data(**kwargs) # get the default context data from the parent class
+        context['num_cards'] = Card.objects.count()
+
+        # get the id of the previously created card
+        context['last_id'] = Card.objects.all().last().id
+        return context
+
+
+
 
 
 
